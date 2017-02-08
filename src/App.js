dbf5829b6122a36957f3f1ecaedf78783a2609be
Jesse Ellis <jessetellis@gmail.com>
2017-02-07 22:58:05 -0500

    Initialize React application

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import {
+  pink500,
+  blue500
+} from 'material-ui/styles/colors'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import AppBar from 'material-ui/AppBar'
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: pink500,
+    accent1Color: blue500
+  }
+});
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <div>
+        <AppBar title='here-ku' />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
